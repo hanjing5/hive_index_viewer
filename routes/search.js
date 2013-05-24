57806bd2fa924e92da70e36cde7cadc853bb0fcd
hjing <hjing@walmartlabs.com>
2013-05-24 16:23:14 -0700

    updated search route

--- routes/search.js
+++ routes/search.js
@@ -21,25 +21,3 @@ exports.results = function(req, res){
   //res.send("respond with a resource");
 };
 
-    var client = new Db('test', new Server("127.0.0.1", 27017, {}), {w: 1}),
-        test = function (err, collection) {
-          collection.insert({a:2}, function(err, docs) {
-
-            collection.count(function(err, count) {
-              test.assertEquals(1, count);
-            });
-
-            // Locate all the entries using find
-            collection.find().toArray(function(err, results) {
-              test.assertEquals(1, results.length);
-              test.assertTrue(results[0].a === 2);
-
-              // Let's close the db
-              client.close();
-            });
-          });
-        };
-
-    client.open(function(err, p_client) {
-      client.collection('test_insert', test);
-    });
