622e5a45f7aceaea0e51da56f084859cb353cffa
Han Jing <hjing@sitep-hnw-rec00.sv.walmartlabs.com>
2014-09-06 16:57:52 -0700

    potentially new way to index hive

---
+++ hive_indexer.js
@@ -0,0 +1,23 @@
+var hive = require('thrift-hive');
+// Client connection
+var client = hive.createClient({
+  version: '0.7.1-cdh3u2',
+  server: '127.0.0.1',
+  port: 10000,
+  timeout: 1000
+});
+// Execute call
+client.execute('use default', function(err){
+  // Query call
+  client.query('show tables')
+  .on('row', function(database){
+    console.log(database);
+  })
+  .on('error', function(err){
+    console.log(err.message);
+    client.end();
+  })
+  .on('end', function(){
+    client.end();
+  });
+});
